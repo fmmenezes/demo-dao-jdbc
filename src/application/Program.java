b00cf41480bde1021b0f4b0a8e9d77dc78e0d704
fmmenezes <fmmenezes@hotmail.com>
2021-03-31 12:26:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department depto = new Department(1, "Books");
+		System.out.println(depto);
+
+		Seller seller = new Seller(1, "Branca Mendes", "branca.mendes@hotmail.com", new Date(), 10000.00, depto);
+		System.out.println(seller);
 
 	}
 
